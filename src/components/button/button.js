bbd35c6b2482ca1eba5ac99020777bbd5d2f5b1b
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-01-13 18:02:42 -0200

    Configure loaders for images and scss
    - Create base button component for test assets
    - Configure loader for scss files
    - Configure file-loader for images

---
+++ src/components/button/button.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './button.scss'
+
+export const Button = () => (
+  <button>Some button</button>
+)
--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,12 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/application.js',
   output: {
-    filename: 'application.js',
+    filename: 'application.[contenthash].js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
@@ -16,11 +17,29 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          process.env.NODE_ENV === 'production'
+            ? MiniCssExtractPlugin.loader
+            : 'style-loader',
+          'css-loader', 'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg)$/,
+        use: [
+          'file-loader'
+        ]
       }
-    ]
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: 'application.[contenthash].css',
+    }),
     new HtmlWebPackPlugin({
       template: './src/index.html',
       filename: './index.html'
