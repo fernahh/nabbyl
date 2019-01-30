83cddfa274b2e8f57a1c1835cdc5ccd037d8dde0
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-01-30 18:57:33 -0200

    Create button component

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -7,4 +7,26 @@ describe('Button Component', () => {
     const wrapper = shallow(<Button />)
     expect(wrapper).toBeDefined()
   })
+
+  it('execute provided function on click', () => {
+    const onButtonClickAction = jest.fn()
+    const wrapper = shallow(<Button onClick={onButtonClickAction} />)
+    wrapper.find('button').simulate('click')
+    expect(onButtonClickAction).toBeCalled()
+  })
+
+  it('render button with submit type', () => {
+    const wrapper = shallow(<Button type="submit" />)
+    expect(wrapper.find('button').props().type).toEqual('submit')
+  })
+
+  it('render button with reset type', () => {
+    const wrapper = shallow(<Button type="reset" />)
+    expect(wrapper.find('button').props().type).toEqual('reset')
+  })
+
+  it('render button with button type', () => {
+    const wrapper = shallow(<Button type="button" />)
+    expect(wrapper.find('button').props().type).toEqual('button')
+  })
 })
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Logo } from '../../components/logo'
 import { Banner } from '../../components/banner'
+import { Button } from '../../components/button'
 
 export const Home = () => (
   <div>
     <Logo />
     <Banner />
+    <Button>Connect with spotify</Button>
   </div>
 )
