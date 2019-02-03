18e12be46b266df7ff6bf2857a14967439275785
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-02-03 19:26:05 -0200

    Create get http helper

---
+++ src/helpers/get/get.test.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import { get } from './index'
+
+jest.mock('axios')
+
+describe('Get', () => {
+  it('call get method from axios', () => {
+    get('http://api.com/dog')
+    expect(axios.get).toBeCalledWith('http://api.com/dog')
+  })
+
+  it('call get method from axios with params', () => {
+    const params = { breed: 'beagle' }
+    get('http://api.com/dog', params)
+    expect(axios.get).toBeCalledWith('http://api.com/dog', {
+      params
+    })
+  })
+})
