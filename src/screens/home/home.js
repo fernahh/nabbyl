91e9b3380e6461bca048e40ce6ebcc99134b07eb
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-02-06 16:52:53 -0200

    Configure grid system and build home screen

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-grid-system'
 import { Logo } from '@src/components/logo'
 import { Banner } from '@src/components/banner'
 import { ConnectButton } from '@src/components/connect-button'
+import './home.scss'
 
 export const Home = () => (
-  <div>
-    <Logo />
-    <Banner />
-    <ConnectButton />
+  <div className="home">
+    <Container justify="center">
+      <Row>
+        <Col>
+          <Logo />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={10} lg={8} xl={7}>
+          <Banner />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} sm={7} md={6} lg={4}>
+          <ConnectButton />
+        </Col>
+      </Row>
+    </Container>
   </div>
 )
