498c2f5b8f84fb584cf6b5bf6d7aaa7a1299bed4
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-02-16 18:07:33 -0200

    Create base album component
    Add gray-color-base to CSS variables
    
    Add album info

--- src/components/album/album.js
+++ src/components/album/album.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { ColorPalette } from '@src/components/color-palette'
 import './album.scss'
 
 export const Album = (props) => {
@@ -10,6 +11,13 @@ export const Album = (props) => {
       <img alt={`Cover of the album ${album.name}`}
         className="album__image"
         src={album.images[0].url} />
+      <div className="album__info">
+        <div className="album__info__content">
+          <span className="album__info__name">{album.name}</span>
+          <span className="album__info__artist">{album.artists[0].name}</span>
+        </div>
+        <ColorPalette colors={album.colors} />
+      </div>
     </div>
   )
 }
--- src/components/album/album.test.js
+++ src/components/album/album.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import { ColorPalette } from '@src/components/color-palette'
 import { Album } from './index'
 
 describe('Album', () => {
@@ -7,7 +8,11 @@ describe('Album', () => {
     name: 'Nevermind',
     images: [
       { url: 'spotify.com/image.jpeg' }
-    ]
+    ],
+    artists: [
+      { name: 'Nirvana' }
+    ],
+    colors: ['#fff', '#000']
   })
 
   it('render with appropriate css class', () => {
@@ -20,5 +25,12 @@ describe('Album', () => {
     const image = wrapper.find('img')
     expect(image.prop('src')).toEqual('spotify.com/image.jpeg')
     expect(image.prop('alt')).toEqual('Cover of the album Nevermind')
+  })
+
+  it('render the album info', () => {
+    const wrapper = shallow(<Album album={getAlbumMock()} />)
+    expect(wrapper.find('.album__info__name').text()).toEqual('Nevermind')
+    expect(wrapper.find('.album__info__artist').text()).toEqual('Nirvana')
+    expect(wrapper.find(ColorPalette).prop('colors')).toEqual(['#fff', '#000'])
   })
 })
