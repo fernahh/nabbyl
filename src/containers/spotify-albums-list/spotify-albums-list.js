4d959cbd62e1e6e966a50158449ff99b08afa3bd
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-03-15 18:09:58 -0300

    Add pagination on spotify albums list component

--- src/containers/spotify-albums-list/spotify-albums-list.js
+++ src/containers/spotify-albums-list/spotify-albums-list.js
@@ -11,21 +11,55 @@ export class SpotifyAlbumsList extends Component {
   }
 
   state = {
-    items: []
+    isRequesting: false,
+    items: [],
+    offset: 0
   }
 
   componentDidMount() {
-    get(`${ENV.API_BASE_URL}/albums`, buildHeaders(this.props.accessToken))
-      .then(response => {
-        this.setState({ items: response.data })
+    this.getAlbums()
+    window.addEventListener('scroll', this.getAlbumsOnScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.getAlbumsOnScroll)
+  }
+
+  getAlbumsOnScroll = () => {
+    if (scrolledEnough() && !this.state.isRequesting)
+      this.getAlbums()
+  }
+
+  getAlbums = () => {
+    this.setState({ 
+      isRequesting: true 
+    })
+
+    get(
+      `${ENV.API_BASE_URL}/albums?offset=${this.state.offset}`, 
+      buildHeaders(this.props.accessToken)
+    ).then(response => {
+      this.setState({ 
+        isRequesting: false,
+        items: this.state.items.concat(response.data),
+        offset: response.headers.offset
       })
+    })
   }
 
   render() {
     return (
       <div className="spotify_albums_list">
-        { this.state.items && <AlbumsList items={this.state.items} /> }
+        { this.state.items && 
+          <AlbumsList items={this.state.items} 
+            isRequesting={this.state.isRequesting} /> }
       </div>
     )
   }
+}
+
+function scrolledEnough() {
+  const scrolledSize = window.innerHeight + window.scrollY
+  const seventyPerCentOfDocument = (document.body.offsetHeight * 70 / 100)
+  return scrolledSize >= seventyPerCentOfDocument
 }
