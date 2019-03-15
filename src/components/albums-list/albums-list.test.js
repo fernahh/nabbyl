09a61002ffe52452f885e09887120e6fbcc2e90b
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-03-15 16:58:04 -0300

    Add loader and transition to album list component

--- src/components/albums-list/albums-list.test.js
+++ src/components/albums-list/albums-list.test.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import { Col } from 'react-grid-system'
+import { Loader } from '@src/components/loader'
+import { Album } from '@src/components/album'
 import { AlbumsList } from './index'
 
 describe('Albums List', () => {
   it('render with appropriate css class', () => {
-    const wrapper = shallow(<AlbumsList items={[]} />)
+    const wrapper = shallow(<AlbumsList items={[]} isRequesting={false} />)
     expect(wrapper.prop('className')).toEqual('albums_list')
   })
 
   it('render a list of albums', () => {
-    const albums = [
+    const items = [
       { add_at: 12345, album: { id: 1, name: 'foo' } }, 
       { add_at: 6789, album: { id: 2, name: 'bar' } }, 
     ]
-    const wrapper = shallow(<AlbumsList items={albums} />)
-    expect(wrapper.find(Col).at(0).key()).toEqual('1')
-    expect(wrapper.find(Col).at(1).key()).toEqual('2')
+    const wrapper = shallow(<AlbumsList items={items} isRequesting={false} />)
+    expect(wrapper.find(Album).at(0).prop('album')).toEqual(items[0].album)
+    expect(wrapper.find(Album).at(1).prop('album')).toEqual(items[1].album)
+  })
+
+  it('render the loader', () => {
+    const wrapper = shallow(<AlbumsList items={[]} isRequesting={true} />)
+    expect(wrapper.find(Loader).length).toBeTruthy()
   })
 })
