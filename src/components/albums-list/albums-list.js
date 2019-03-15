c6c63022df7885c7161c0edf412e65f98b626221
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-03-15 16:58:04 -0300

    Add loader and transition to album list component

--- src/components/albums-list/albums-list.js
+++ src/components/albums-list/albums-list.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Col, Row } from 'react-grid-system'
+import { TransitionGroup } from 'react-transition-group'
+import { FadeTransition } from '@src/components/fade-transition'
 import { Album } from '@src/components/album'
+import { Loader } from '@src/components/loader'
+import './albums-list.scss'
 
 export const AlbumsList = props => {
-  const { items } = props
+  const { isRequesting, items } = props
 
   return (
-    <Row className="albums_list">
+    <TransitionGroup component={Row} className="albums_list">
       {items.map(item => (
-        <Col xs={6} sm={4} md={3} key={item.album.id}>
-          <Album album={item.album} />
-        </Col>
+        <FadeTransition key={item.album.id}>
+          <Col xs={6} sm={4} md={3} key={item.album.id}>
+            <Album album={item.album} />
+          </Col>
+        </FadeTransition>
       ))}
-    </Row>
+      { isRequesting && 
+        <FadeTransition>
+          <Col>
+            <Loader />
+          </Col> 
+        </FadeTransition> }
+    </TransitionGroup>
   )
 }
 
 AlbumsList.propTypes = {
+  isRequesting: PropTypes.bool.isRequired,
   items: PropTypes.array.isRequired
 }
