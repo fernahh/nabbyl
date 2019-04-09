5cf1dd12dd426875af0db606cc0c117838d42a8e
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-04-09 17:33:05 -0300

    Add dynamic size for logo component

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './logo.scss'
 
-export const Logo = () => (
-  <div className="logo">
-    <div className="logo__cover"></div>
-    <span className="logo__word">nabbyl</span>
-  </div>
-)
+export const Logo = props => {
+  const size = `${props.size}px`
+  const borderWidth = `${(props.size * 9 / 100)}px`
+
+  return (
+    <div
+      className="logo"
+      style={{
+        height: size
+      }}>
+      <div
+        className="logo__cover"
+        style={{
+          height: size,
+          width: size,
+          borderWidth
+        }} />
+      <span
+        className="logo__word"
+        style={{
+          fontSize: `${(props.size / 2)}px`,
+          lineHeight: size
+        }}>
+        nabbyl
+      </span>
+    </div>
+  )
+}
+
+Logo.propTypes = {
+  size: PropTypes.number
+}
+
+Logo.defaultProps = {
+  size: 90
+}
