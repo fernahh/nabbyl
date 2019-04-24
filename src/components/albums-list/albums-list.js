5b991b5223af1b4fbd221e51e66137724764ec23
Luiz Fernando Rodrigues <fernahh@gmail.com>
2019-04-24 16:40:15 -0300

    Add notification error on albums list component

--- src/components/albums-list/albums-list.js
+++ src/components/albums-list/albums-list.js
@@ -1,35 +1,40 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Col, Row } from 'react-grid-system'
 import { TransitionGroup } from 'react-transition-group'
 import { FadeTransition } from '@src/components/fade-transition'
 import { Album } from '@src/components/album'
 import { Loader } from '@src/components/loader'
+import { Notification } from '@src/components/notification'
 import './albums-list.scss'
 
 export const AlbumsList = props => {
-  const { isRequesting, items } = props
+  const { isRequesting, hasError, items } = props
 
   return (
-    <TransitionGroup component={Row} className="albums_list">
-      {items.map(item => (
-        <FadeTransition key={item.album.id}>
-          <Col xs={6} sm={4} md={3} key={item.album.id}>
-            <Album album={item.album} />
-          </Col>
-        </FadeTransition>
-      ))}
-      { isRequesting && 
-        <FadeTransition>
-          <Col>
-            <Loader />
-          </Col> 
-        </FadeTransition> }
-    </TransitionGroup>
+    <Fragment>
+      { hasError && <Notification /> }
+      <TransitionGroup component={Row} className="albums_list">
+        {items.length && items.map(item => (
+          <FadeTransition key={item.album.id}>
+            <Col xs={6} sm={4} md={3} key={item.album.id}>
+              <Album album={item.album} />
+            </Col>
+          </FadeTransition>
+        ))}
+        { isRequesting &&
+          <FadeTransition>
+            <Col>
+              <Loader />
+            </Col> 
+          </FadeTransition> }
+      </TransitionGroup>
+    </Fragment>
   )
 }
 
 AlbumsList.propTypes = {
   isRequesting: PropTypes.bool.isRequired,
-  items: PropTypes.array.isRequired
+  items: PropTypes.array.isRequired,
+  hasError: PropTypes.bool
 }
